bc0b7d42c9e6d8f5220f54e2c4944d25e2bd8aaa
maskman0974-cmyk <maskman0974@gmail.com>
2026-04-01 16:29:51 +0200

    V2.6

--- gamedata.h
+++ gamedata.h
@@ -1,59 +1,39 @@
 #ifndef GAMEDATA_H
 #define GAMEDATA_H
 
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-// Structuur voor een aanval
 typedef struct {
     char naam[16];
     int kracht;
     int minLvl;
-    int effect;
-    int effectChance; 
+    int effect;       // 0: Normal, 1: Fire/Ki, 2: Special
+    int effectChance; // Wordt nu gebruikt voor Type ID in UI
 } Move;
 
-// Structuur voor een karakter
 typedef struct {
+    char naam[16];
     int char_id;
-    char naam[20];
     int lvl;
-    int xp;
-    int xp_nodig;
     int hp;
     int max_hp;
+    int xp;
+    int xp_nodig;
+    int status; // 0: Base, 1: G2, 2: G4, 3: G5 etc.
     bool isGevuld;
-    int status; 
     Move moves[4];
     const uint16_t* battle_front_bitmap;
     const uint16_t* battle_back_bitmap;
 } Karakter;
 
-// Externe variabelen voor Items
-extern char* itemNamen[5];
-extern int itemHeal[5];
-extern int itemAantal[5];
-
-// Externe variabelen voor Teams en PC
+// Declaraties voor extern gebruik
 extern Karakter team[6];
 extern Karakter vijand_team[6];
-extern Karakter pcBox[30];
-
-// Externe variabelen voor de game state
-extern int activeEnemyIdx;
 extern int activeIdx;
-extern int regio;
+extern int activeEnemyIdx;
+extern char* typeNamen[5];
 
-// Functie declaraties
-void setMove(Move* m, const char* naam, int kracht, int minLvl, int effect, int chance);
-int getEmptyPartySlot(void);
-int getEmptyPcSlot(void);
-void healWholeTeam(void);
-bool checkAliveTeam(void);
-void checkNewMoves(Karakter* k);
-bool checkEvolutie(Karakter* k);
-void initKarakter(Karakter* k, int id, int lvl);
-void initTeam(void);
 int getBaseType(int char_id);
 
 #endif
