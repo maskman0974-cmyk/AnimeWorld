cfa2ae427ad257a472522dc634f2bc9d942a7a6d
maskman0974-cmyk <maskman0974@gmail.com>
2026-04-01 16:33:37 +0200

    V2.6

--- gamedata.h
+++ gamedata.h
@@ -4,14 +4,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Struct voor aanvallen
 typedef struct {
     char naam[16];
     int kracht;
     int minLvl;
-    int effect;       // 0: Normal, 1: Fire/Ki, 2: Special
-    int effectChance; // Wordt nu gebruikt voor Type ID in UI
+    int type; // 0:FY, 1:KI, 2:CH, 3:HA, 4:DF
 } Move;
 
+// Struct voor je helden en vijanden
 typedef struct {
     char naam[16];
     int char_id;
@@ -20,20 +21,22 @@ typedef struct {
     int max_hp;
     int xp;
     int xp_nodig;
-    int status; // 0: Base, 1: G2, 2: G4, 3: G5 etc.
+    int status; // 0:Base, 1:Evo1, 2:Evo2
     bool isGevuld;
     Move moves[4];
-    const uint16_t* battle_front_bitmap;
-    const uint16_t* battle_back_bitmap;
 } Karakter;
 
-// Declaraties voor extern gebruik
+// Global variabelen die overal gebruikt worden
 extern Karakter team[6];
 extern Karakter vijand_team[6];
 extern int activeIdx;
 extern int activeEnemyIdx;
+extern int regio;
+extern int stage;
 extern char* typeNamen[5];
 
-int getBaseType(int char_id);
+// Prototypes
+void initKarakter(Karakter* k, int id, int lvl);
+void checkEvolutie(Karakter* k);
 
 #endif
