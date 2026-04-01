fa1ed04a86eb26209698b13ba6cf2d7bf456f8e2
maskman0974-cmyk <maskman0974@gmail.com>
2026-04-01 16:39:07 +0200

    V2.6

--- gamedata.h
+++ gamedata.h
@@ -4,6 +4,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Toetsen definities voor de hele game
+#define KEY_A      0x0001
+#define KEY_B      0x0002
+#define KEY_SELECT 0x0004
+#define KEY_START  0x0008
+#define KEY_RIGHT  0x0010
+#define KEY_LEFT   0x0020
+#define KEY_UP     0x0040
+#define KEY_DOWN   0x0080
+#define KEY_R      0x0100
+#define KEY_L      0x0200
+
 // Struct voor aanvallen
 typedef struct {
     char naam[16];
@@ -12,7 +24,7 @@ typedef struct {
     int type; // 0:FY, 1:KI, 2:CH, 3:HA, 4:DF
 } Move;
 
-// Struct voor je helden en vijanden
+// Struct voor je helden en vijanden (volledige versie)
 typedef struct {
     char naam[16];
     int char_id;
@@ -35,8 +47,10 @@ extern int regio;
 extern int stage;
 extern char* typeNamen[5];
 
-// Prototypes
+// Prototypes voor globaal gebruik
 void initKarakter(Karakter* k, int id, int lvl);
 void checkEvolutie(Karakter* k);
+void initTeam();
+int getBaseType(int char_id);
 
 #endif
