4768b005b557137df45af9fc5fac38e2acedabcb
maskman0974-cmyk <maskman0974@gmail.com>
2026-04-02 10:07:45 +0100

    V2.6 fix

--- gamedata.h
+++ gamedata.h
@@ -1,10 +1,10 @@
 #ifndef GAMEDATA_H
 #define GAMEDATA_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-// Toetsen definities voor de hele game
+// Toetsen (Verwijder ze dus uit main.c!)
 #define KEY_A      0x0001
 #define KEY_B      0x0002
 #define KEY_SELECT 0x0004
@@ -16,41 +16,50 @@
 #define KEY_R      0x0100
 #define KEY_L      0x0200
 
-// Struct voor aanvallen
 typedef struct {
     char naam[16];
     int kracht;
     int minLvl;
-    int type; // 0:FY, 1:KI, 2:CH, 3:HA, 4:DF
+    int effect;
+    int effectChance; 
 } Move;
 
-// Struct voor je helden en vijanden (volledige versie)
 typedef struct {
-    char naam[16];
     int char_id;
+    char naam[20];
     int lvl;
-    int hp;
-    int max_hp;
     int xp;
     int xp_nodig;
-    int status; // 0:Base, 1:Evo1, 2:Evo2
+    int hp;
+    int max_hp;
     bool isGevuld;
+    int status; 
     Move moves[4];
+    const uint16_t* battle_front_bitmap;
+    const uint16_t* battle_back_bitmap;
 } Karakter;
 
-// Global variabelen die overal gebruikt worden
+extern char* itemNamen[5];
+extern int itemHeal[5];
+extern int itemAantal[5];
+
 extern Karakter team[6];
 extern Karakter vijand_team[6];
-extern int activeIdx;
-extern int activeEnemyIdx;
-extern int regio;
-extern int stage;
-extern char* typeNamen[5];
+extern Karakter pcBox[30];
 
-// Prototypes voor globaal gebruik
+extern int activeEnemyIdx;
+extern int activeIdx;
+extern int regio;
+
+void setMove(Move* m, const char* naam, int kracht, int minLvl, int effect, int chance);
+int getEmptyPartySlot(void);
+int getEmptyPcSlot(void);
+void healWholeTeam(void);
+bool checkAliveTeam(void);
+void checkNewMoves(Karakter* k);
+bool checkEvolutie(Karakter* k);
 void initKarakter(Karakter* k, int id, int lvl);
-void checkEvolutie(Karakter* k);
-void initTeam();
+void initTeam(void);
 int getBaseType(int char_id);
 
 #endif
